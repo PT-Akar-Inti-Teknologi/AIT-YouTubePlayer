dcbc339b32b4c4ab06927f99d7c3f588f53da038
Elbek Khasanov <94638543+xasanovelbek@users.noreply.github.com>
2023-07-31 20:53:55 +0500

    Update YouTubePlayer.swift

--- YouTubePlayer/YouTubePlayer/YouTubePlayer.swift
+++ YouTubePlayer/YouTubePlayer/YouTubePlayer.swift
@@ -240,10 +240,10 @@ open class YouTubePlayerView: UIView, WKNavigationDelegate {
     fileprivate func loadWebViewWithParameters(_ parameters: YouTubePlayerParameters) {
 
         // Get HTML from player file in bundle
-        let rawHTMLString = htmlStringWithFilePath(playerHTMLPath())!
+        guard let rawHTMLString = htmlStringWithFilePath(playerHTMLPath()) else { return }
 
         // Get JSON serialized parameters string
-        let jsonParameters = serializedJSON(parameters as AnyObject)!
+        guard let jsonParameters = serializedJSON(parameters as AnyObject) else { return }
 
         // Replace %@ in rawHTMLString with jsonParameters string
         let htmlString = rawHTMLString.replacingOccurrences(of: "%@", with: jsonParameters)
